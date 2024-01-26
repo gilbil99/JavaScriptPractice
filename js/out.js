8f76751cb2e4c03dd7f1e7fe9684adf5ede6903a
HongKilDong <golbol@naver.com>
2024-01-26 18:04:02 +0900

    javaSCript  연습

---
+++ js/out.js
@@ -0,0 +1,14 @@
+// out.js
+//함수: 기능을 정의한다
+//function 함수명([매개변수]){}
+function hello(name){
+
+    alert("Hello?" + name);
+}
+
+//함수명: go
+//매개변수
+function go(){
+
+    location.href="http://naver.com";
+}
---
+++ js/out2.js
@@ -0,0 +1,43 @@
+//out.js
+// function 빨래하다(){
+//     console.log("==============");
+//     console.log("1.먼지 털자");
+//     console.log("2. 물에 넣자");
+//     console.log("3.비누를 바른다");
+//     console.log("4.오물조물 비빈다");
+//     console.log("5.물에 행군다");
+//     console.log("6.짠다");
+//     console.log("7.넌다");
+// }//----------------------
+
+// const 청소하다=function(){
+//     document.write("<hr color='red'> </hr>");
+//     document.write("<ol>");
+//     document.write(`
+//     <li>로봇 청소기를 돌리다</li>
+//     <li>물걸레로 닦다</li>
+//     `);
+//     document.write("</ol");
+
+
+// }
+function 여행하다(){
+    document.write("유후우우우우우 ");
+    document.write("일본 도착");
+
+}
+function 쉬다(){
+    document.write("<hr color='red'> </hr>");
+    document.write("<ol>");
+    document.write(`
+    <li>침대에 눕는다</li>
+    <li>눈을 감는다</li>
+    <li>잔다</li>
+    `);
+    document.write("</ol");
+}
+    
+    
+    
+    
+    //-----------------------------------------
